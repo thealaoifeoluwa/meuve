cc080d243ab9285b27db852567a9168aeb020c0a
Alao Ifeoluwa <alaoifeoluwa@gmail.com>
2022-08-07 23:45:06 +0100

    working on media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
+
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1350px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+
+      'xs': {'max': '475px'},
+// => @media (max-width: 639px) { ... }
+    },
+
     colors: {
       'body': '#FFFFFF',
       'orange': '#FFC300',
